bbf732b222dd2bb05f6c786ee61a9cf06a628172
Bryan Clark <bryan@bclark.net>
2022-04-08 08:28:10 -0400

    complete anecdotes project (exercises 1.12-1.14)

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -12,16 +12,39 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [popular, setPopular] = useState(anecdotes[selected])
+  const votesCopy = [...votes]
+  const [popIndex, setPopIndex] = useState(0)
 
   const rndNum = () => {
-    setSelected(Math.round((Math.random() * 10000 )) % 7)
+    setSelected(Math.round((Math.random() * 10000 )) % anecdotes.length)
+    getPopular()
+  }
+
+  const vote = () => {
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
+    getPopular()
+  }
+
+  function getPopular() {
+    const max = Math.max(...votes)
+    const index = votes.indexOf(max)
+    setPopular(anecdotes[index])
+    setPopIndex(index)
   }
 
   return (
     <div>
       {anecdotes[selected]}
+      <p>this anecdote has {votes[selected]} votes</p>
       <p></p>
+      <button onClick={vote}>vote</button>
       <button onClick={rndNum}>next anecdote</button>
+      <h2>Anecdote with most votes</h2>
+      <p>{popular}</p>
+      <p>has {votes[popIndex]} votes</p>
     </div>
   )
 }
